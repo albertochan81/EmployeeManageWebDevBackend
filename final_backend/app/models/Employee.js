2fb20d9b562f391baefb1024b84b4e929129a47a
albertochan81 <alberto.chanliu81@myhunter.cuny.edu>
2023-05-07 11:25:07 -0400

    Backend edit
    New routes, controllers and config

--- final_backend/app/models/Employee.js
+++ final_backend/app/models/Employee.js
@@ -1,21 +1,17 @@
-const Sequelize = require('sequelize');
-const db = require('../config/database');
-
-const Employee = db.define('employee',{
-    first_name: {
+module.exports = (sequelize, Sequelize) => {
+    const Employee = sequelize.define("employee", {
+      first_name: {
         type: Sequelize.STRING
-    },
-    last_name: {
+      },
+      last_name: {
         type: Sequelize.STRING
-    },
-    department: {
+      },
+      department: {
         type: Sequelize.STRING
-    }, 
-
-})
-// added extra
-console.log(Employee === db.models.Employee); 
-console.log(Employee);
-console.log(db.Employee);
-
-module.exports = Employee;
+      }
+    });
+  
+    return Employee;
+  };
+  
+  
